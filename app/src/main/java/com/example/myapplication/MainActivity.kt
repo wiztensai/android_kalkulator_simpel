c41185d3ba9a5540ac3c5a05470481cc82e79cec
wiz <wiztensai@gmail.com>
2021-03-03 03:22:58 +0700

    update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // tes 2
+        val etOmset = findViewById<TextView>(R.id.etOmset)
+        val etCost = findViewById<TextView>(R.id.etCost)
+        val btnHitung = findViewById<TextView>(R.id.btnHitung)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+
+        btnHitung.setOnClickListener {
+            val omset = etOmset.text.toString().toInt()
+            val cost = etCost.text.toString().toInt()
+
+            val result = omset - cost
+            tvResult.text = "Hasil: IDR " + result.toString() +",-"
+        }
     }
 }
